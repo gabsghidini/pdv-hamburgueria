70bf814d8aa0fc623a3dd7771b0371e24b70d795
Gabs Ghidini <gabs.ghidini@xnetweb,com.br>
2024-11-09 10:41:42 -0300

    FEAT: render list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,46 @@
+async function getItems() {
+    const items = await fetch('../items.json')
+        .then(response => response.json())  // Converte a resposta para um objeto JS
+        .then(items => {
+            return items
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo JSON:', error);
+        });
+
+    return items;
+}
+
+/* BUTTON FUNCTIONS */
 function viewCart() {
     console.log("ver carrinho");
 }
 
-function clearCart () {
+function clearCart() {
     console.log("limpar carrinho");
 }
+
+/* RENDER FUNCTIONS */
+async function renderItems() {
+    const itemsUl = document.getElementById("MainContent__Section--ItemsList");
+    
+    const items = await getItems()
+    
+    const itemsList = items.map((item) => {
+        const li = document.createElement("li");
+        
+        li.innerHTML = `
+        <img src=${item.img}>
+        <h1>${item.name}</h1>
+        <h2>R$ ${item.price.toFixed(2).replace(".", ",")}</h2>
+        <h3>${item.type}</h3>
+        `
+        return li;
+    })
+
+    itemsList.forEach(element => {
+        itemsUl.appendChild(element);
+    });
+}
+
+renderItems()
